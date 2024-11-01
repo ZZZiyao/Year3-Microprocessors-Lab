e6de3bc3aee9dcc0b648071c17396afed958d125
jy3122 <jy3122@skbl-418-008.ph.ic.ac.uk>
2024-11-01 09:48:19 +0000

    after thursday

--- main.s
+++ main.s
@@ -1,11 +1,10 @@
 	#include <xc.inc>
-
+ 
 psect	code, abs
-	
 main:
 	org	0x0
 	goto	start
-
+ 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
@@ -16,9 +15,9 @@ loop:
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movf 	PORTD, W, A
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
-
+ 
 	end	main
