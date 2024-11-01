8c66f46746d054f8254078eafa6f5feee31765aa
jy3122 <jy3122@skbl-418-008.ph.ic.ac.uk>
2024-11-01 12:11:20 +0000

    led

--- main.s
+++ main.s
@@ -11,31 +11,58 @@ main:
 setup:	
 	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
+	clrf    TRISC,A ;clear
 	goto	start
 	; ******* My data and where to put it in RAM *
 myTable:
-	db	'T','h','i','s',' ','i','s',' ','j','u','s','t'
-	db	' ','s','o','m','e',' ','d','a','t','a'
-	myArray EQU 0x400	; Address in RAM for data
+	db 0b10101010
+	db 0b01010101
+	db 0b11110000
+	db 0b00001111
+	db 0b11001100
+	db 0b00110011
+	db 0b11111111
+	db 0b00000000
+	
+	dataLength  EQU  8
+	;myArray EQU 0x400	Address in RAM for data
 	counter EQU 0x10	; Address of counter variable
 	align	2		; ensure alignment of subsequent instructions 
 	; ******* Main programme *********************
+	
 start:	
-	lfsr	0, myArray	; Load FSR0 with address in RAM	
+	;lfsr	0, myArray	Load FSR0 with address in RAM	
 	movlw	low highword(myTable)	; address of data in PM
 	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
 	movwf	TBLPTRH, A	; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	22		; 22 bytes to read
+	movlw	dataLength		; 8 bytes to read
 	movwf 	counter, A	; our counter register
 loop:
         tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
+	movff	TABLAT, PORTC	; move read data from TABLAT to (FSR0), increment FSR0	
+	
+	movlw 0xFF
+	movwf 0x10
+	movlw 0xFF
+	movwf 0x11
+	
+	call    bigdelay        ;will be defined later
 	decfsz	counter, A	; count down to zero
 	bra	loop		; keep going until finished
 	
 	goto	0
+	
 
+bigdelay:
+    movlw    0x00               ;w=0
+
+Dloop:
+    decf 0x11,f,A
+    subwfb 0x10,f,A
+    bc Dloop               ;if carry, then loop again
+    return
+    
 	end	main
