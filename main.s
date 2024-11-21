eedeebeec8306fbf2e4a79bcdebd3fde3c83b3df
zx2222 <zx2222@skbl-418-008.ph.ic.ac.uk>
2024-11-21 11:41:04 +0000

    shibaichangshi

--- main.s
+++ main.s
@@ -1,30 +1,36 @@
 #include <xc.inc>
-
-extrn	Keypad_Setup, Keypad_read_input, Keypad_decode  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_D
-	
-psect	udata_acs   ; reserve data space in access ram
-Keypad_result:  ds   1
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
-psect main_code, class=CODE
-    
+ 
+; Declare external subroutines
+extern    Keypad_Setup, find_column, find_row, combine, find_key
+extern    LCD_Setup, LCD_Write_Message, LCD_Send_Byte_D
+ 
+; Data section for storing variables in access RAM
+psect    udata_acs   
+result: ds  1   ; Variable to store the keypad result
+delay_count: ds 1      ; Variable for delay routine counter
+ 
+; Code section for main program
+psect    main_code, class=CODE
+; Main program entry point
 Main_loop:
-    call Keypad_Setup
-    call LCD_Setup
-    
+    call Keypad_Setup     ; Initialize the keypad
+    call LCD_Setup        ; Initialize the LCD display
+ 
 Main_Loop_Start:
-    call Keypad_read_input
-    movf Keypad_result, W, A
-    call LCD_Send_Byte_D
-    movlw 0xFF
-    movwf delay_count, A
-    call delay
-    goto Main_Loop_Start
-    
-    
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
-
-	end
+    call find_column      ; Identify which column is pressed
+    call find_row         ; Identify which row is pressed
+    call combine          ; Combine results from row and column
+    call find_key         ; Decode the pressed key
+    movf result, W, A     ; Move the detected key to the W register for display
+    call LCD_Send_Byte_D    ; Send the detected key to the LCD display
+    movlw 0xFF            ; Load 0xFF into W for delay counter initialization
+    movwf delay_count, A  ; Initialize delay counter
+    call delay            ; Call delay subroutine
+    goto Main_Loop_Start  ; Repeat the main loop
+; Delay subroutine
+delay:
+    decfsz delay_count, A ; Decrement the delay counter until zero
+    bra delay             ; Loop until counter is zero
+    return                ; Return from delay subroutine
+ 
+    end
