6da57c03028129c793201da6de33fb91988200db
zx2222 <zx2222@skbl-418-008.ph.ic.ac.uk>
2024-11-22 12:50:13 +0000

    Thur afternoon

--- main.s
+++ main.s
@@ -1,48 +1,37 @@
 #include <xc.inc>
- 
 ; Declare external subroutines
 extrn    Keypad_Setup, find_column, find_row, combine, find_key
 extrn    LCD_Setup, LCD_Write_Message, LCD_Send_Byte_D
- 
 ; Data section for storing variables in access RAM
 psect    udata_acs   
 result: ds  1   ; Variable to store the keypad result
 delay_count: ds 1      ; Variable for delay routine counter
- 
 ; Code section for main program
 psect	code, abs	
 rst: 	org 0x0
- 	goto	setup
-
+	goto	setup
+ 
 ; Main program entry point
 setup:
     clrf result,A
     call Keypad_Setup     ; Initialize the keypad
     call LCD_Setup        ; Initialize the LCD display
- 
+    goto Main_Loop_Start
+    
 Main_Loop_Start:
     call find_column      ; Identify which column is pressed
     call find_row         ; Identify which row is pressed
     call combine          ; Combine results from row and column
-    ;call find_key         ; Decode the pressed key
-    movf result, W, A     ; Move the detected key to the W register for display
-    bz clear_display
-    call find_key
-    movf result, W, A
-    call LCD_Send_Byte_D    ; Send the detected key to the LCD display
-    movlw 0xFF            ; Load 0xFF into W for delay counter initialization
-    movwf delay_count, A  ; Initialize delay counter
-    call delay            ; Call delay subroutine
+    call find_key         ; Decode the pressed key
+    call LCD_Send_Byte_D   ; Send the detected key to the LCD display
+    ;movlw 0xFF            ; Load 0xFF into W for delay counter initialization
+    ;movwf delay_count, A  ; Initialize delay counter
+    ;call delay            ; Call delay subroutine
     goto Main_Loop_Start  ; Repeat the main loop
-; Delay subroutine
     
-clear_display:
-    movlw 0x01
-    call LCD_Send_Byte_D
-    goto Main_Loop_Start
+; Delay subroutine
 delay:
     decfsz delay_count, A ; Decrement the delay counter until zero
     bra delay             ; Loop until counter is zero
     return                ; Return from delay subroutine
- 
     end rst
